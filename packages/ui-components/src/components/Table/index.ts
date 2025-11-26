c9b9d9a70e5ab04492bfd14fcc616b82ecf7a5c6
jpfisher72 <jpfisher72@gmail.com>
2025-11-26 11:49:35 -0800

    Export module augmentation from "@mui/x-data-grid-premium/themeAugmentation"
    Allows overriding datagrid default styles and props in consuming application without transiently depending on @mui/x-data-grid-premium

--- packages/ui-components/src/components/Table/index.ts
+++ packages/ui-components/src/components/Table/index.ts
@@ -3,3 +3,5 @@ export { Table }
 export type * from "./types"
 export * from '@mui/x-data-grid-premium';
 export type * from "@mui/x-data-grid-premium"
+// The following allows `import type {} from "@weng-lab/ui-components"` which is needed to add DataGrid default styles/props to themes
+export type {} from "@mui/x-data-grid-premium/themeAugmentation"
