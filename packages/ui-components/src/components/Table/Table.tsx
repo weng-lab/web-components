47dc808af2ea368bea0650327ca64edf756373dc
jpfisher72 <jpfisher72@gmail.com>
2026-03-08 20:44:43 -0700

    Remove autosizeOnMount
    Fixes #50 (hopefully)

--- packages/ui-components/src/components/Table/Table.tsx
+++ packages/ui-components/src/components/Table/Table.tsx
@@ -127,7 +127,6 @@ const CustomDataGridPremium = (props: Omit<TableProps, "emptyTableFallback" | "e
         apiRef={apiRef}
         columns={transformedColumns}
         rows={rows}
-        autosizeOnMount
         autosizeOptions={autosizeOptions}
         disableRowSelectionOnClick
         showToolbar={showToolbar}
