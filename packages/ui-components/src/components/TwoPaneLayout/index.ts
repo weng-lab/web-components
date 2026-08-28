12597acc3cec6961bff2baff3a53d002e395aa98
mlacadie <lacadiem14@gmail.com>
2026-08-28 14:00:28 -0400

    expose autosort

--- packages/ui-components/src/components/TwoPaneLayout/index.ts
+++ packages/ui-components/src/components/TwoPaneLayout/index.ts
@@ -1,4 +1,5 @@
 export { default as TwoPaneLayout } from "./TwoPaneLayout";
 export { useTablePlotSync } from "./useTablePlotSync";
 export { useSyncedTable } from "./useSyncedTable";
+export type { SyncedTableProps } from "./useSyncedTable";
 export type { TwoPanePlotConfig, TwoPaneLayoutProps, Direction, ResponsiveDirection } from "./types";
